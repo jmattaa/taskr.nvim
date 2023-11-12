7ac52a23609a5970ca194f42dbdf9f73436cb376
jmattaa <mattajonathan1@gmail.com>
2023-11-12 22:37:14 +0100

    Displaying tasks
    make it prettier and move  to the according file
    
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

--- lua/taskr/tasks.lua
+++ lua/taskr/tasks.lua
@@ -3,7 +3,7 @@ local M = {}
 local config = require "taskr.config".config
 local utils = require "taskr.utils"
 
-local _tasks = utils.table.load_file(config.taskfile) or {}
+M.tasks = utils.table.load_file(config.taskfile) or {}
 
 function M.add_task(description, apply)
     if not description or not apply then
@@ -15,10 +15,9 @@ function M.add_task(description, apply)
         return
     end
 
-    table.insert(_tasks,
+    table.insert(M.tasks,
         { description, apply }
     )
-    M.tasks = _tasks
 end
 
 function M.add_current_file()
@@ -34,6 +33,7 @@ function M.add_current_file()
     end
 
     local description = vim.fn.input("Task description: ")
+    vim.cmd [[redraw]]
     if description == "" then return end
 
     M.add_task(description, filename)
@@ -41,6 +41,7 @@ end
 
 function M.save_tasks()
     utils.table.save_file(M.tasks, config.taskfile)
+    print("Taskr: Saved tasks")
 end
 
 -- didn't take this from quickmark.nvim
@@ -57,19 +58,27 @@ local function print_to_buf(buf, str, startl)
     )
 end
 
+local function print_desc_apply_to_buf(buf, desc, apply, startl)
+    local line = string.format("%-40s\t%s", desc, apply)
+    print_to_buf(buf, line, startl)
+end
+
 function M.display_tasks()
     local buf = vim.api.nvim_create_buf(false, true)
 
-    print_to_buf(buf, "Hello", 0)
-    print_to_buf(buf, "Hello", 1)
-    print_to_buf(buf, "Hello", 2)
-    print_to_buf(buf, "Taskr nvim stuff", 3)
+    for i = 1, #M.tasks do
+        local current_line = i - 1
+        local desc = M.tasks[i][1]
+        local apply = M.tasks[i][2]
+
+        print_desc_apply_to_buf(buf, desc, apply, current_line)
+    end
 
     local keymap_opts = {
         nowait = true, noremap = true, silent = true
     }
 
-    vim.api.nvim_buf_set_keymap(buf, 'n', 'q', ':bdelete<CR>', keymap_opts)
+    vim.api.nvim_buf_set_keymap(buf, "n", "q", ":bdelete<CR>", keymap_opts)
 
     -- this must be right before we open the window
     -- so we can programatically do stuff
